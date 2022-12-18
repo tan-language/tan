a3988ec6bc0359db39d4503f07f8ad16c9f459a0
George Moschovitis <gmosx@reizu.com>
2022-12-18 21:10:10 +0200

    Minor

--- src/ann.rs
+++ src/ann.rs
@@ -3,4 +3,17 @@ use std::collections::HashMap;
 pub type Ann = HashMap<String, String>;
 
 // #TODO consider Anned?
+#[derive(Debug)]
 pub struct Annotated<T>(T, Ann);
+
+impl<T> Annotated<T> {
+    pub fn new(value: T) -> Self {
+        Self(value, HashMap::new())
+    }
+}
+
+impl<T> AsRef<T> for Annotated<T> {
+    fn as_ref(&self) -> &T {
+        &self.0
+    }
+}
