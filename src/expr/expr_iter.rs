6b76774709d8755699dce11a42c83d3e574e359e
George Moschovitis <gmosx@reizu.com>
2023-01-12 21:23:45 +0200

    Fix ExprIter

--- src/expr/expr_iter.rs
+++ src/expr/expr_iter.rs
@@ -16,6 +16,10 @@ impl Ann<Expr> {
 // #Insight
 // The iterator is implemented as a separate struct, for flexibility.
 
+// #TODO implement owned iterator
+// #TODO implement mutable iterator
+// #TODO https://aloso.github.io/2021/03/09/creating-an-iterator
+
 /// A depth-first Expr iterator.
 #[derive(Default)]
 pub struct ExprIter<'a> {
@@ -38,38 +42,41 @@ impl<'a> Iterator for ExprIter<'a> {
             },
             Some(Ann(Expr::List(children), ..)) => {
                 self.children = &self.children[1..];
-
-                // start iterating the sub-trees
+                // iterate the sub-trees
                 *self = ExprIter {
                     children: children.as_slice(),
                     parent: Some(Box::new(mem::take(self))),
                 };
                 self.next()
             }
-            _ => self.children.get(0),
+            _ => {
+                let x = self.children.get(0);
+                self.children = &self.children[1..];
+                x
+            }
         }
     }
 }
 
-// #TODO implement the depth-first iterator
-// #TODO implement owned iterator
-// #TODO implement mutable iterator
-// #TODO https://aloso.github.io/2021/03/09/creating-an-iterator
-
 #[cfg(test)]
 mod tests {
-    use crate::{
-        api::eval_string,
-        eval::{env::Env, prelude::setup_prelude},
-    };
+    use crate::{lexer::Lexer, parser::Parser};
 
     #[test]
     fn expr_iter_performs_depth_first_iteration() {
-        let env = &mut setup_prelude(Env::new());
-        let input = "(quot (1 2 3 (4 5) (6 (7 8)) 9 10))";
-        let expr = eval_string(input, env).unwrap();
-        let value = expr.to_string();
-        let expected = "(1 2 3 (4 5) (6 (7 8)) 9 10)";
-        assert_eq!(value, expected);
+        // let env = &mut setup_prelude(Env::new());
+        let input = "(quot (1 2 3 (4 5) (6 (+ 7 8)) 9 10))";
+
+        let mut lexer = Lexer::new(input);
+        let tokens = lexer.lex().unwrap();
+
+        let mut parser = Parser::new(tokens);
+        let expr = parser.parse().unwrap();
+
+        let terms: Vec<String> = expr.iter().map(|ax| ax.0.to_string()).collect();
+        let expected_terms = vec![
+            "quot", "1", "2", "3", "4", "5", "6", "+", "7", "8", "9", "10",
+        ];
+        assert_eq!(terms, expected_terms);
     }
 }
