3f607517daebb5b3952bb98d61d606af578da08d
George Moschovitis <gmosx@reizu.com>
2023-01-12 22:04:55 +0200

    Cleanup

--- src/api.rs
+++ src/api.rs
@@ -9,8 +9,7 @@ use crate::{
     parser::Parser,
 };
 
-// #TODO all should return Ranged<Error> and Ann<Expr>.
-
+// #TODO all should return Ranged<Error>.
 /// Parses a Tan expression encoded as a text string.
 pub fn parse_string(input: impl AsRef<str>) -> Result<Ann<Expr>, Error> {
     let input = input.as_ref();
@@ -24,15 +23,10 @@ pub fn parse_string(input: impl AsRef<str>) -> Result<Ann<Expr>, Error> {
     Ok(expr)
 }
 
+// #TODO should return Ranged<Error> and Ann<Expr>.
 /// Evaluates a Tan expression encoded as a text string.
 pub fn eval_string(input: impl AsRef<str>, env: &mut Env) -> Result<Expr, Error> {
-    let input = input.as_ref();
-
-    let mut lexer = Lexer::new(input);
-    let tokens = lexer.lex()?;
-
-    let mut parser = Parser::new(tokens);
-    let expr = parser.parse()?;
+    let expr = parse_string(input)?;
 
     let value = eval(expr, env)?;
 
