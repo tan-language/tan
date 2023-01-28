d2bc9498998eb41fee06c764568e4660486175d5
George Moschovitis <gmosx@reizu.com>
2023-01-28 17:36:30 +0200

    Minor

--- src/api.rs
+++ src/api.rs
@@ -20,7 +20,6 @@ pub fn parse_string(input: impl AsRef<str>) -> std::result::Result<Ann<Expr>, Ve
 
     let mut lexer = Lexer::new(input);
     let tokens = lexer.lex()?;
-    // let tokens = lexer.lex().unwrap();
 
     let mut parser = Parser::new(tokens);
     let expr = parser.parse()?;
