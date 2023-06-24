a75df01a85aaeaa0a213a6e2c235bc2c48f222eb
George Moschovitis <gmosx@reizu.com>
2023-06-24 12:42:14 +0300

    Implement Iterator trait

--- src/util/lookahead_iterator.rs
+++ src/util/lookahead_iterator.rs
@@ -1,20 +1,14 @@
+// #TODO implement the Iterator interface!
+
 pub struct LookaheadIterator<'a, T> {
     items: std::slice::Iter<'a, T>,
     lookahead: Vec<&'a T>, // #TODO find a better name.
 }
 
-// #TODO explain what this does.
-impl<'a, T> LookaheadIterator<'a, T> {
-    pub fn new(items: &'a [T]) -> Self {
-        Self {
-            items: items.iter(),
-            lookahead: Vec::new(),
-        }
-    }
+impl<'a, T> Iterator for LookaheadIterator<'a, T> {
+    type Item = &'a T;
 
-    // #TODO unit test
-    // #TODO refactor
-    pub fn next(&mut self) -> Option<&'a T> {
+    fn next(&mut self) -> Option<Self::Item> {
         // First try to exhaust the lookahead buffer, containing items that
         // where 'peeked' and the 'put back' to the buffer.
         if let Some(item) = self.lookahead.pop() {
@@ -27,9 +21,21 @@ impl<'a, T> LookaheadIterator<'a, T> {
             None
         }
     }
+}
+
+// #TODO explain what this does.
+impl<'a, T> LookaheadIterator<'a, T> {
+    pub fn new(items: &'a [T]) -> Self {
+        Self {
+            items: items.iter(),
+            lookahead: Vec::new(),
+        }
+    }
 
     // #TODO find a better name.
     pub fn put_back(&mut self, item: &'a T) {
         self.lookahead.push(item);
     }
 }
+
+// #TODO add unit tests.
