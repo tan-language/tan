09f59af7039211a8491324e8d0b6caa2699a1ac8
George Moschovitis <gmosx@reizu.com>
2023-07-13 09:03:54 +0300

    Support absolute module paths (#6)
    * Support absolute module paths, resolve in TAN_ROOT
    
    * More improvements in the module system

--- src/eval/util.rs
+++ src/eval/util.rs
@@ -17,6 +17,9 @@ use super::eval;
 
 // #todo split read_module, eval(_module)
 
+// #todo not sure that dir as module is a good idea.
+// #todo automatically add last path component as prefix, like Go.
+
 // // #TODO map module name to path
 // // #TODO resolve crate.x.y, or this.x.y
 // pub fn module_path_from_name(name: &str) -> String {
@@ -37,7 +40,12 @@ use super::eval;
 // (use "@std/math" :as "math")
 // (use std.math (pi tau))
 
-pub fn canonicalize_module_path(path: impl AsRef<Path>, context: &Context) -> String {
+// #todo handle module_urls with https://, https://, ipfs://, etc, auto-download, like Deno.
+
+pub fn canonicalize_module_path(
+    path: impl AsRef<Path>,
+    context: &Context,
+) -> std::io::Result<String> {
     let mut path = path.as_ref().to_string_lossy().into_owned();
 
     // #TODO what is a good coding convention for 'system' variables?
@@ -51,19 +59,23 @@ pub fn canonicalize_module_path(path: impl AsRef<Path>, context: &Context) -> St
         // Canonicalize the path using the current-module-path as the base path.
         if path.starts_with("./") {
             path = format!("{base_path}{}", path.strip_prefix(".").unwrap());
-        } else if !path.starts_with("/") {
+        } else if path.starts_with("/") {
+            path = format!("{}{path}", context.root_path);
+        } else {
             // #TODO consider not supporting this, always require the "./"
             path = format!("{base_path}/{}", path);
         }
     }
 
-    path.clone()
+    // #TODO move the canonicalize to the canonicalize_module_path function?
+    let path = PathBuf::from(path).canonicalize()?;
+
+    Ok(path.to_string_lossy().to_string())
 }
 
 // #TODO add unit test.
-pub fn compute_module_file_paths(path: impl AsRef<Path>) -> std::io::Result<Vec<String>> {
-    // #TODO move the canonicalize to the canonicalize_module_path function?
-    let module_path = path.as_ref().canonicalize()?;
+pub fn compute_module_file_paths(module_path: impl AsRef<Path>) -> std::io::Result<Vec<String>> {
+    let module_path = module_path.as_ref();
 
     let mut module_file_paths: Vec<String> = Vec::new();
     // let mut buf: PathBuf;
@@ -122,9 +134,12 @@ pub fn compute_module_file_paths(path: impl AsRef<Path>) -> std::io::Result<Vec<
 
 pub fn eval_module(path: impl AsRef<Path>, context: &mut Context) -> Result<Expr, Vec<Error>> {
     // #todo support import_map style rewriting
-    // #TODO more general solution needed.
 
-    let module_path = canonicalize_module_path(&path, context);
+    let result = canonicalize_module_path(&path, context);
+
+    let Ok(module_path) = result else {
+        return Err(vec![result.unwrap_err().into()]);
+    };
 
     let module_name = strip_tan_extension(&module_path);
 
