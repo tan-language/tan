f7d8f9a7a91d883646ce426a4fdb4d05d8f73334
George Moschovitis <gmosx@reizu.com>
2023-07-17 19:36:27 +0300

    Refactoring (#10)

--- src/ops/special.rs
+++
@@ -1,6 +0,0 @@
-//! Special forms
-
-// #TODO primitives, builtins
-// #TODO cannot be implemented with Expr::ForeignFunc as the args are pre-evaluated.
-
-// #Insight special-form != primitive ? special form requires special case in the interpreter?
