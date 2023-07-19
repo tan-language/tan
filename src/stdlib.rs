0176fab96ffe8735903f11af5206499671c05dfe
George Moschovitis <gmosx@reizu.com>
2023-07-19 23:04:41 +0300

    Introduce a simple facility for random numbers

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -1,11 +1,15 @@
 pub mod fs;
 pub mod io;
+pub mod math;
 pub mod prelude;
 pub mod process;
+pub mod rng;
 
 use crate::context::Context;
 
-use self::{fs::setup_std_fs, prelude::setup_std_prelude, process::setup_std_process};
+use self::{
+    fs::setup_std_fs, prelude::setup_std_prelude, process::setup_std_process, rng::setup_std_rand,
+};
 
 // #todo consider removing the `std` prefix from module paths, like haskell.
 // #todo find a better prefix than setup_
@@ -15,5 +19,6 @@ use self::{fs::setup_std_fs, prelude::setup_std_prelude, process::setup_std_proc
 pub fn setup_std(context: &mut Context) {
     setup_std_fs(context);
     setup_std_process(context);
+    setup_std_rand(context);
     setup_std_prelude(context);
 }
---
+++ src/stdlib/rng.rs
@@ -0,0 +1,46 @@
+// #insight random is _not_ part of math!
+
+// #todo random_int, random_float
+// #todo should take a range trait.
+// #todo support random-number-generator 'object', with more specialization
+// #todo fast, crypto-secure, very-random, per-thread, etc versions.
+// #todo but also a couple of helper functions.
+// #todo better module name: stochastic, rnd, rng? `rng` is interesting, nah 'rng' is not great.
+
+// #todo use OnceLock to cache the RNG
+
+use std::{rc::Rc, sync::Arc};
+
+use crate::{context::Context, error::Error, expr::Expr, module::Module};
+use rand::Rng;
+
+/// (random 100) returns a random integer in the range 0..100
+pub fn random_int(args: &[Expr], _context: &Context) -> Result<Expr, Error> {
+    if let Some(end) = args.first() {
+        let Some(end) = end.as_int() else {
+            return Err(Error::invalid_arguments("expected Int argument", end.range()));
+        };
+
+        let mut rng = rand::thread_rng();
+
+        Ok(Expr::Int(rng.gen_range(0..end)))
+    } else {
+        Err(Error::invalid_arguments(
+            "expected at least one argument",
+            None,
+        ))
+    }
+}
+
+pub fn setup_std_rand(context: &mut Context) {
+    let module = Module::new("rng", context.top_scope.clone());
+
+    let scope = &module.scope;
+
+    scope.insert("random", Expr::ForeignFunc(Arc::new(random_int)));
+
+    // #todo this is a hack.
+    let module_path = format!("{}/std/rng", context.root_path);
+    // #todo introduce a helper for this.
+    context.module_registry.insert(module_path, Rc::new(module));
+}
