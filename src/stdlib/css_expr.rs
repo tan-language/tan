368602bcc8b808bfdcdfc8c2428565ddf38abc49
George Moschovitis <gmosx@reizu.com>
2023-11-12 19:10:20 +0200

    feat: css-expr supports nested CSS

--- src/stdlib/css_expr.rs
+++ src/stdlib/css_expr.rs
@@ -38,10 +38,13 @@ fn render_css_expr(expr: &Expr) -> Result<Expr, Error> {
 
                 while i < terms.len() {
                     let prop = render_css_expr(&terms[i])?; // #todo no render needed
-                    let mut declaration = format!("{}: ", format_value(&prop));
+                    let mut declaration = format_value(&prop);
                     i += 1;
-                    let value = render_css_expr(&terms[i])?; // #todo no render needed
-                    declaration.push_str(&format_value(&value));
+                    if i < terms.len() {
+                        declaration.push_str(": ");
+                        let value = render_css_expr(&terms[i])?; // #todo no render needed
+                        declaration.push_str(&format_value(&value));
+                    }
                     i += 1;
                     body.push(declaration);
                 }
@@ -121,6 +124,59 @@ mod tests {
         let expr = eval_string(input, &mut context).unwrap();
         let value = expr.as_string().unwrap();
         let expected = r#"body { margin-top: 0; margin-bottom: 10px; background: red }"#;
+        assert_eq!(value, expected);
+
+        let input = r#"
+            (use "/dialect/css-expr")
+
+            (let expr #CSS-Expr
+                '(body
+                    margin-top: "0"
+                    margin-bottom: "10px"
+                    background: "red"
+
+                    (main
+                        (div.profile
+                            background-color: "yellow"
+                        )
+                    )
+                )
+            )
+
+            (css-expr/to-css expr)
+        "#;
+        let mut context = Context::new();
+        let expr = eval_string(input, &mut context).unwrap();
+        let value = expr.as_string().unwrap();
+        let expected = r#"body { margin-top: 0; margin-bottom: 10px; background: red }"#;
+        assert_eq!(value, expected);
+    }
+
+    #[test]
+    fn to_expr_to_css_supports_nested_css() {
+        let input = r#"
+            (use "/dialect/css-expr")
+
+            (let expr #CSS-Expr
+                '(body
+                    margin-top: "0"
+                    margin-bottom: "10px"
+                    background: "red"
+
+                    (main
+                        (div.profile
+                            background-color: "yellow"
+                        )
+                    )
+                )
+            )
+
+            (css-expr/to-css expr)
+        "#;
+        let mut context = Context::new();
+        let expr = eval_string(input, &mut context).unwrap();
+        let value = expr.as_string().unwrap();
+        let expected = r#"body { margin-top: 0; margin-bottom: 10px; background: red; main { div.profile { background-color: yellow } } }"#;
         assert_eq!(value, expected);
     }
 }
