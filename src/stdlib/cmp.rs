1fbfc77370832a4e7f125233543cc173c0331435
Georgios Moschovitis <gmosx@reizu.com>
2024-01-10 21:13:27 +0200

    refactor: resolve a clippy warning

--- src/stdlib/cmp.rs
+++ src/stdlib/cmp.rs
@@ -7,11 +7,26 @@ pub fn rust_ordering_from_tan_ordering(tan_ordering: &Expr) -> Option<Ordering> 
         return None;
     };
 
-    if *ordering < 0 {
-        Some(Ordering::Less)
-    } else if *ordering > 0 {
-        Some(Ordering::Greater)
-    } else {
-        Some(Ordering::Equal)
+    Some(ordering.cmp(&0))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use assert_matches::assert_matches;
+
+    use crate::{expr::Expr, stdlib::cmp::rust_ordering_from_tan_ordering};
+
+    #[test]
+    fn rust_ordering_from_tan_ordering_usage() {
+        assert_matches!(
+            rust_ordering_from_tan_ordering(&Expr::Int(20)),
+            Some(Ordering::Greater)
+        );
+        assert_matches!(
+            rust_ordering_from_tan_ordering(&Expr::Int(0)),
+            Some(Ordering::Equal)
+        );
     }
 }
