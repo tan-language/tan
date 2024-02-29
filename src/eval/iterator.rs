9ffd2c82dbc294201be8808e7643a74b5dc204d8
Georgios Moschovitis <gmosx@reizu.com>
2024-02-29 21:51:09 +0200

    feat: add an iterator for dicts (nasty)

--- src/eval/iterator.rs
+++ src/eval/iterator.rs
@@ -5,7 +5,7 @@
 
 use std::{cell::RefCell, rc::Rc};
 
-use crate::expr::Expr;
+use crate::expr::{expr_clone, Expr};
 
 pub trait ExprIterator {
     fn next(&mut self) -> Option<Expr>;
@@ -94,26 +94,25 @@ impl<'a> ExprIterator for ArrayIteratorRc<'a> {
     }
 }
 
-// // #todo under construction.
-// pub struct DictIterator<'a> {
-//     current: usize,
-//     items: std::cell::Ref<'a, HashMap<String, Expr>>,
-//     pub step: usize,
-// }
+// #todo under construction.
+pub struct DictIterator {
+    current: usize,
+    items: Vec<Expr>,
+    pub step: usize,
+}
 
-// impl<'a> ExprIterator for DictIterator<'a> {
-//     // #todo keep rust iterator instead.
-//     fn next(&mut self) -> Option<Expr> {
-//         // if self.current < self.items.len() {
-//         //     let value = self.items[self.current].clone(); // #todo argh, avoid this. should array have Rcs? SOS!!!
-//         //     self.current += self.step;
-//         //     Some(value)
-//         // } else {
-//         //     None
-//         // }
-//         todo!()
-//     }
-// }
+impl ExprIterator for DictIterator {
+    // #todo keep rust iterator instead.
+    fn next(&mut self) -> Option<Expr> {
+        if self.current < self.items.len() {
+            let value = self.items[self.current].clone(); // #todo argh, avoid this. should array have Rcs? SOS!!!
+            self.current += self.step;
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
 
 // #todo find better name.
 // #todo consider using Box<dyn ExprIterator> instead, at least have a custom helper that returns Box.
@@ -157,14 +156,29 @@ pub fn try_iterator_from<'a>(expr: &'a Expr) -> Option<Rc<RefCell<dyn ExprIterat
                 step: 1,
             })))
         }
-        // Expr::Dict(items) => {
-        //     let items = items.borrow();
-        //     Some(Rc::new(RefCell::new(DictIterator {
-        //         current: 0,
-        //         items,
-        //         step: 1,
-        //     })))
-        // }
+        Expr::Dict(_) => {
+            // example usage:
+            // (let user {:name "George" :age :gender :male})
+            // (for [f user] (writeln "*** ${(f 0)} = ${(f 1)}"))
+            // #todo somehow reuse dict_get_entries
+            let Some(items) = expr.as_dict_mut() else {
+                panic!("invalid state in for-dict");
+            };
+
+            // #todo why does map return k as String?
+            // #todo wow, this is incredibly inefficient.
+            // #todo #hack temp fix we add the a `:` prefix to generate keys
+            let items: Vec<_> = items
+                .iter()
+                .map(|(k, v)| Expr::array(vec![Expr::KeySymbol(k.clone()), expr_clone(v)]))
+                .collect();
+
+            Some(Rc::new(RefCell::new(DictIterator {
+                current: 0,
+                items,
+                step: 1,
+            })))
+        }
         _ => None,
     }
 }
