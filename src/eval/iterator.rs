a8689be7d3ec13e74b4a67de10aab80ccf8c01b6
George Moschovitis <george.moschovitis@gmail.com>
2024-03-30 10:00:59 +0200

    feat: reimplement Set, add iterator support, temp code (#49)
    * feat: reimplement Set
    
    * feat: iterator for set

--- src/eval/iterator.rs
+++ src/eval/iterator.rs
@@ -118,7 +118,6 @@ impl<'a> ExprIterator for ArrayIteratorRc<'a> {
     }
 }
 
-// #todo under construction.
 pub struct MapIterator {
     current: usize,
     items: Vec<Expr>,
@@ -126,6 +125,27 @@ pub struct MapIterator {
 }
 
 impl ExprIterator for MapIterator {
+    // #todo keep rust iterator instead.
+    fn next(&mut self) -> Option<Expr> {
+        if self.current < self.items.len() {
+            let value = self.items[self.current].clone(); // #todo argh, avoid this. should array have Rcs? SOS!!!
+            self.current += self.step;
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
+// #todo move iterator impls to the corresponding type impl?
+
+pub struct SetIterator {
+    current: usize,
+    items: Vec<Expr>,
+    pub step: usize,
+}
+
+impl ExprIterator for SetIterator {
     // #todo keep rust iterator instead.
     fn next(&mut self) -> Option<Expr> {
         if self.current < self.items.len() {
@@ -196,6 +216,24 @@ pub fn try_iterator_from<'a>(expr: &'a Expr) -> Option<Rc<RefCell<dyn ExprIterat
                 .collect();
 
             Some(Rc::new(RefCell::new(MapIterator {
+                current: 0,
+                items,
+                step: 1,
+            })))
+        }
+        Expr::Set(_) => {
+            // example usage: #todo
+            // #todo somehow reuse map_get_entries
+            let Some(items) = expr.as_set_mut() else {
+                panic!("invalid state in for-set");
+            };
+
+            // #todo wow, this is incredibly inefficient.
+            // #todo #hack temp fix we add the a `:` prefix to generate keys
+            // #todo argh, cloned!
+            let items: Vec<_> = items.iter().cloned().collect();
+
+            Some(Rc::new(RefCell::new(SetIterator {
                 current: 0,
                 items,
                 step: 1,
