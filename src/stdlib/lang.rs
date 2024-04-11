4b28117d91d7738f32daacdb206a8cd2dd9b9657
Georgios Moschovitis <gmosx@reizu.com>
2024-04-11 20:43:23 +0300

    chore: minor

--- src/stdlib/lang.rs
+++ src/stdlib/lang.rs
@@ -72,6 +72,7 @@ pub fn debug_expr(args: &[Expr], _context: &mut Context) -> Result<Expr, Error> 
     Ok(Expr::string(s))
 }
 
+// #todo rename to nil?/is-nil?
 // #insight with dynamic typing you don't strictly need a Maybe type?
 pub fn is_none(args: &[Expr], _context: &mut Context) -> Result<Expr, Error> {
     let [expr] = args else {
