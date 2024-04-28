14b97329b3c06620a86497883a417e331b5fe58e
Georgios Moschovitis <gmosx@reizu.com>
2024-04-28 12:34:33 +0300

    feat: introduce Complex

--- src/library/math.rs
+++ src/library/math.rs
@@ -1,1 +1,9 @@
-// #todo
+use crate::context::Context;
+
+use self::complex::setup_lib_math_complex;
+
+pub mod complex;
+
+pub fn setup_lib_math(context: &mut Context) {
+    setup_lib_math_complex(context);
+}
--- src/library/math/complex.rs
+++ src/library/math/complex.rs
@@ -5,13 +5,32 @@
 // (let z (Complex 1.0 0.3))
 // (let r (* z z))
 
-pub fn complex_new(_args: &[Expr], _context: &mut Context) -> Result<Expr, Error> {
-    // #todo use an array for the moment.
-    let set: HashSet<Expr> = HashSet::new();
-    Ok(Expr::set(set))
+use std::sync::Arc;
+
+use crate::{
+    context::Context,
+    error::Error,
+    expr::{annotate_type, Expr},
+    util::module_util::require_module,
+};
+
+// #todo how to extend to quaternions and octonions.
+
+// (Complex)
+// (Complex re)
+// (Complex re im)
+pub fn complex_new(args: &[Expr], _context: &mut Context) -> Result<Expr, Error> {
+    // #todo consider a ForeignStruct.
+
+    let re = args.first().unwrap_or_else(|| &Expr::Float(0.0));
+    let im = args.get(1).unwrap_or_else(|| &Expr::Float(0.0));
+
+    // #todo optimize.
+    let expr = Expr::array(vec![re.clone(), im.clone()]);
+    Ok(annotate_type(expr, "Complex"))
 }
 
-pub fn setup_lib_complex(context: &mut Context) {
+pub fn setup_lib_math_complex(context: &mut Context) {
     // #todo skip the `math/` prefix?
     let module = require_module("math/complex", context);
 
