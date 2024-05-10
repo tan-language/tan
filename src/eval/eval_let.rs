fbfdb30b2ef4362fe8679ac8ccc2d598acea1771
George Moschovitis <george.moschovitis@gmail.com>
2024-05-10 08:34:05 +0300

    feat: first cut of pass-through annotations for let (#65)
    * fix: maintain let annotations after macro_expand
    
    * feat: pass-through annotation in let
    
    * fix: set correct Expr::Type
    
    * fix: fix multiple issues

--- src/eval/eval_let.rs
+++ src/eval/eval_let.rs
@@ -2,12 +2,16 @@ use crate::{context::Context, error::Error, expr::Expr};
 
 use super::{eval, insert_binding};
 
-pub fn eval_let(args: &[Expr], context: &mut Context) -> Result<Expr, Error> {
+// #todo pass-through the annotations.
+
+pub fn eval_let(op: &Expr, args: &[Expr], context: &mut Context) -> Result<Expr, Error> {
     // #todo there is currently no resolver, duh.
     // #todo actually some resolving is happening in macro_expand, e.g. checking for binding values.
     // #todo this is already parsed statically by resolver, no need to duplicate the tests here?
     // #todo also report some of these errors statically, maybe in a sema phase?
     // #todo use 'location' or 'lvalue' instead of name?
+
+    // #insight 'pass-through' let annotations, only for ...def.
 
     let mut args = args.iter();
 
@@ -21,7 +25,7 @@ pub fn eval_let(args: &[Expr], context: &mut Context) -> Result<Expr, Error> {
             break;
         };
 
-        let value = eval(value, context)?;
+        let value = Expr::maybe_annotated(eval(value, context)?, op.annotations());
 
         insert_binding(name, value, context)?
     }
--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ use rust_decimal::prelude::*;
 
 use crate::{
     error::{Error, ErrorVariant},
-    expr::{annotate, annotate_range, Expr},
+    expr::{annotate, annotate_range, format_value, Expr},
     lexer::{
         token::{Token, TokenKind},
         Lexer,
@@ -175,14 +175,16 @@ impl<'a> Parser<'a> {
                     if is_type_expression {
                         // #todo #IMPORTANT verify that the type expression is valid
                         // #todo investigate if some part of the annotation is missing from ann_expr!
-                        expr = annotate(expr, "type", ann_expr.clone());
+                        expr = annotate(expr, "type", Expr::Type(format_value(ann_expr)));
                     } else {
-                        // #todo convert to multiple annotations
-                        // #todo iterate the map
-                        // #todo #IMPORTANT implement me! this is placeholder.
                         let Some(ann_list) = ann_expr.as_list() else {
-                            // #todo report error!
-                            eprintln!("ERROR in annotation ERROR");
+                            let mut error = Error::new(ErrorVariant::MalformedAnnotation);
+                            error.push_note(
+                                &format!("cannot parse `{}`", annotation_token.lexeme()),
+                                Some(annotation_token.range()),
+                            );
+                            self.errors.push(error);
+                            // Ignore the buffered annotations, and continue parsing to find more syntactic errors.
                             return expr;
                         };
                         let mut i = 1;
