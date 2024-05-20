c849087ea48b85fe2960cc48a6dafa1c4df1d360
Georgios Moschovitis <gmosx@reizu.com>
2024-05-20 21:38:59 +0300

    refactor: compute signature from invocable type, hackish

--- src/util/method.rs
+++ src/util/method.rs
@@ -7,18 +7,35 @@ use crate::{
     expr::{format_value, Expr},
 };
 
+// #todo write unit test!
 // #todo automatically infer the signature from type annotations.
 // #insight only apply on invocable exprs.
 pub fn compute_signature_from_annotations(expr: &Expr) -> Option<String> {
     // #todo the signature annotation is super-low-level, remove?
-    if let Some(signature) = expr.annotation("signature") {
-        // #todo validate that signature is a string.
-        let signature = format_value(signature);
-        // #todo this is temp convention!
-        Some(format!("$${signature}"))
+    if let Some(typ) = expr.annotation("type") {
+        // #todo add some error checking for the type annotation here.
+        // #todo temp hack solution, only handles (Func [...] ...) types.
+        let typ = format_value(typ);
+        let typ = typ.replace(['(', ')'], "");
+        let parts: Vec<&str> = typ.split(' ').collect();
+        let mut typ = parts[1..parts.len() - 1].join("$$");
+        // #todo #hack ultra nasty!
+        if typ.starts_with("Array") {
+            typ = typ.chars().skip(5 /* "Array".len() */).collect();
+        }
+        Some(typ)
     } else {
         None
     }
+
+    // // if let Some(signature) = expr.annotation("signature") {
+    // //     // #todo validate that signature is a string.
+    // //     let signature = format_value(signature);
+    // //     // #todo this is temp convention!
+    // //     Some(format!("$${signature}"))
+    // // } else {
+    //     None
+    // }
 }
 
 // #todo signature should also encode the return type!!
@@ -45,4 +62,10 @@ pub fn compute_dyn_signature(args: &[Expr], context: &Context) -> String {
     }
 
     signature.join("$$")
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn compute_signature_from_annotations_usage() {}
 }
