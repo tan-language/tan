94016c312b8931d59350067571f651461c4bd261
Georgios Moschovitis <gmosx@reizu.com>
2024-07-11 23:10:27 +0300

    Add Float/from-string. #feat

--- src/library/float.rs
+++ src/library/float.rs
@@ -4,7 +4,10 @@ use crate::{
     context::Context,
     error::Error,
     expr::Expr,
-    util::{args::unpack_bool_arg, module_util::require_module},
+    util::{
+        args::{unpack_bool_arg, unpack_stringable_arg},
+        module_util::require_module,
+    },
 };
 
 // #todo Implement with Tan.
@@ -31,6 +34,18 @@ pub fn float_from_bool(args: &[Expr], _context: &mut Context) -> Result<Expr, Er
     Ok(Expr::Float(if value { 1.0 } else { 0.0 }))
 }
 
+// #todo Consider (Float/from-string ...)
+pub fn float_from_string(args: &[Expr], _context: &mut Context) -> Result<Expr, Error> {
+    let string = unpack_stringable_arg(args, 0, "string")?;
+    let Ok(value) = string.parse::<f64>() else {
+        return Err(Error::invalid_arguments(
+            &format!("string=`{string}` is not a valid Float number"),
+            args[0].range(),
+        ));
+    };
+    Ok(Expr::Float(value))
+}
+
 pub fn setup_lib_float(context: &mut Context) {
     // #todo put in 'float' path, and import selected functionality to prelude.
     let module = require_module("prelude", context);
@@ -41,4 +56,8 @@ pub fn setup_lib_float(context: &mut Context) {
     module.insert("Float", Expr::ForeignFunc(Arc::new(float_from_int)));
     module.insert("Float$$Int", Expr::ForeignFunc(Arc::new(float_from_int)));
     module.insert("Float$$Bool", Expr::ForeignFunc(Arc::new(float_from_bool)));
+    module.insert(
+        "Float$$String",
+        Expr::ForeignFunc(Arc::new(float_from_string)),
+    );
 }
