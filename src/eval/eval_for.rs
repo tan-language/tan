e70b4f41d9b804ae19bcfa853b0c3d87ac6c327f
Georgios Moschovitis <gmosx@reizu.com>
2024-08-12 18:00:15 +0300

    Introduce a fucked up version of list comprehensions. #wip

--- src/eval/eval_for.rs
+++ src/eval/eval_for.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{cell::RefCell, rc::Rc, sync::Arc};
 
 use crate::{
     context::Context,
@@ -7,18 +7,36 @@ use crate::{
     scope::Scope,
 };
 
-use super::{eval, insert_binding, iterator::try_iterator_from};
+use super::{
+    eval, insert_binding,
+    iterator::{try_iterator_from, try_iterator_from_consuming, ExprIterator},
+};
 
 // #insight
-// `while` is a generalization of `if`
-// `for` is a generalization of `let`
-// `for` is related with `do`
-// `for` is monadic
+// - `while` is a generalization of `if`
+// - `for` is a generalization of `let`
+// - `for` is related with `do`
+// - `for` is monadic
 
 // #todo check racket.
 // #todo implement for->list, for->map, for->fold, etc.
 
 // #todo what should happen if variable source is nil?
+
+fn insert_next_bindings(
+    bindings: &[(&Expr, Rc<RefCell<dyn ExprIterator>>)],
+    context: &mut Context,
+) -> Result<bool, Error> {
+    for (var, iterator) in bindings {
+        let mut iterator = iterator.borrow_mut();
+        if let Some(value) = iterator.next() {
+            insert_binding(var, value, context)?;
+        } else {
+            return Ok(false);
+        }
+    }
+    Ok(true)
+}
 
 // (for [x 10] (writeln x))
 pub fn eval_for(args: &[Expr], context: &mut Context) -> Result<Expr, Error> {
@@ -34,6 +52,8 @@ pub fn eval_for(args: &[Expr], context: &mut Context) -> Result<Expr, Error> {
     let binding = args.first().unwrap();
     let body = &args[1..];
 
+    // #todo Optimize the standard case with one binding!
+
     // #todo should check both for list and array (i.e. as_iterable)
     let Some(binding_parts) = binding.as_array() else {
         // #todo proper error!
@@ -43,34 +63,74 @@ pub fn eval_for(args: &[Expr], context: &mut Context) -> Result<Expr, Error> {
         ));
     };
 
-    // #todo support _multiple_ bindings.
-    let [var, value] = &binding_parts[..] else {
-        return Err(Error::invalid_arguments(
-            "invalid for binding",
-            binding.range(),
-        ));
-    };
+    let mut i = 0;
 
-    // #insight for the ListIterator
-    let value = eval(value, context)?;
+    // let mut bindings2: [&Expr;10] = [&BINDINGS_SENTINEL;10];
+    let mut bindings2 = Vec::with_capacity(binding_parts.len() / 2);
 
-    // #todo also handle (Range start end step)
-    // #todo maybe step should be external to Range, or use SteppedRange, or (Step-By (Range T))
-    let Some(iterator) = try_iterator_from(&value) else {
-        // #todo proper error!
-        return Err(Error::invalid_arguments(
-            &format!("invalid for binding, `{value}` is not iterable"),
-            value.range(),
-        ));
-    };
+    while i < binding_parts.len() {
+        let var = binding_parts.get(i).unwrap();
+        let value = binding_parts.get(i + 1).unwrap();
+
+        let value = eval(value, context)?;
+
+        // #todo also handle (Range start end step)
+        // #todo maybe step should be external to Range, or use SteppedRange, or (Step-By (Range T))
+        let Some(iterator) = try_iterator_from_consuming(value) else {
+            // #todo proper error!
+            return Err(Error::invalid_arguments(
+                "invalid for binding, not iterable",
+                // value.range(),
+                None,
+            ));
+        };
+
+        bindings2.push((var, iterator));
+
+        i += 2;
+    }
+
+    // #todo Use Rust's chunks.
+    // for i in 0..binding_parts.len() {
+    //     let var = bindings.get(i).unwrap();
+    //     if let Some(value) = bindings.get(i+1).unwrap()
+    //     insert_binding(var, value, context)?;
+    // }
+
+    // #insight If the binding iterables have different length, iterate until
+    // the shortest is exhausted, similar to how Rust's zip and Python list
+    // comprehensions work.
+
+    // // #todo support _multiple_ bindings.
+    // let [var, value] = &binding_parts[..] else {
+    //     return Err(Error::invalid_arguments(
+    //         "invalid for binding",
+    //         binding.range(),
+    //     ));
+    // };
+
+    // // #insight for the ListIterator
+    // let value = eval(value, context)?;
+
+    // // #todo also handle (Range start end step)
+    // // #todo maybe step should be external to Range, or use SteppedRange, or (Step-By (Range T))
+    // let Some(iterator) = try_iterator_from(&value) else {
+    //     // #todo proper error!
+    //     return Err(Error::invalid_arguments(
+    //         &format!("invalid for binding, `{value}` is not iterable"),
+    //         value.range(),
+    //     ));
+    // };
 
     let prev_scope = context.scope.clone();
     context.scope = Arc::new(Scope::new(prev_scope.clone()));
 
-    let mut iterator = iterator.borrow_mut();
+    // let mut iterator = iterator.borrow_mut();
 
-    'outer_loop: while let Some(value) = iterator.next() {
-        insert_binding(var, value, context)?;
+    // 'outer_loop: while let Some(value) = iterator.next() {
+    //     insert_binding(var, value, context)?;
+    'outer_loop: while insert_next_bindings(&bindings2, context)? {
+        // insert_binding(var, value, context)?;
         'inner_loop: for expr in body {
             match eval(expr, context) {
                 Err(Error {
--- src/eval/iterator.rs
+++ src/eval/iterator.rs
@@ -5,7 +5,11 @@
 
 // #todo what about negative iteration, negative step?
 
-use std::{cell::RefCell, rc::Rc, sync::RwLockReadGuard};
+use std::{
+    cell::RefCell,
+    rc::Rc,
+    sync::{Arc, RwLock, RwLockReadGuard},
+};
 
 use crate::{
     expr::{expr_clone, Expr},
@@ -121,6 +125,26 @@ impl<'a> ExprIterator for ArrayIteratorRc<'a> {
     }
 }
 
+pub struct ArrayIteratorRc2 {
+    current: usize,
+    items: Arc<RwLock<Vec<Expr>>>,
+    pub step: usize,
+}
+
+impl ExprIterator for ArrayIteratorRc2 {
+    fn next(&mut self) -> Option<Expr> {
+        let items = expect_lock_read(&self.items);
+
+        if self.current < items.len() {
+            let value = items[self.current].clone(); // #todo avoid this, should array have Rcs? SOS!!!
+            self.current += self.step;
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
 pub struct MapIterator {
     current: usize,
     items: Vec<Expr>,
@@ -202,6 +226,94 @@ pub fn try_iterator_from<'a>(expr: &'a Expr) -> Option<Rc<RefCell<dyn ExprIterat
             Some(Rc::new(RefCell::new(ArrayIteratorRc {
                 current: 0,
                 items,
+                step: 1,
+            })))
+        }
+        Expr::Map(_) => {
+            // example usage:
+            // (let user {:name "George" :age :gender :male})
+            // (for [f user] (writeln "*** ${(f 0)} = ${(f 1)}"))
+            // #todo somehow reuse map_get_entries
+            let Some(items) = expr.as_map_mut() else {
+                panic!("invalid state in for-map");
+            };
+
+            // #todo why does map return k as String?
+            // #todo wow, this is incredibly inefficient.
+            // #todo #hack temp fix we add the a `:` prefix to generate keys
+            let items: Vec<_> = items
+                .iter()
+                .map(|(k, v)| Expr::array(vec![Expr::KeySymbol(k.clone()), expr_clone(v)]))
+                .collect();
+
+            Some(Rc::new(RefCell::new(MapIterator {
+                current: 0,
+                items,
+                step: 1,
+            })))
+        }
+        Expr::Set(_) => {
+            // example usage: #todo
+            // #todo somehow reuse map_get_entries
+            let Some(items) = expr.as_set_mut() else {
+                panic!("invalid state in for-set");
+            };
+
+            // #todo wow, this is incredibly inefficient.
+            // #todo #hack temp fix we add the a `:` prefix to generate keys
+            // #todo try to avoid the cloned!
+            let items: Vec<_> = items.iter().cloned().collect();
+
+            Some(Rc::new(RefCell::new(SetIterator {
+                current: 0,
+                items,
+                step: 1,
+            })))
+        }
+        _ => None,
+    }
+}
+
+// #todo Only this version is useful, remove the non-consuming one!
+pub fn try_iterator_from_consuming(expr: Expr) -> Option<Rc<RefCell<dyn ExprIterator>>> {
+    match expr.unpack() {
+        Expr::Int(n) => Some(Rc::new(RefCell::new(IntRangeIterator {
+            current: 0,
+            end: *n,
+            step: 1,
+        }))),
+        Expr::IntRange(start, end, step) => Some(Rc::new(RefCell::new(IntRangeIterator {
+            // #todo start is not really needed, could use just current!
+            current: *start,
+            end: *end,
+            step: *step,
+        }))),
+        Expr::Float(n) => Some(Rc::new(RefCell::new(FloatRangeIterator {
+            current: 0.0,
+            end: *n,
+            step: 1.0,
+        }))),
+        Expr::FloatRange(start, end, step) => Some(Rc::new(RefCell::new(FloatRangeIterator {
+            // #todo start is really not needed!
+            current: *start,
+            end: *end,
+            step: *step,
+        }))),
+        // #todo consolidate handling of List and Array.
+        // Expr::List(items) => Some(Rc::new(RefCell::new(ArrayIterator {
+        //     current: 0,
+        //     items,
+        //     step: 1,
+        // }))),
+        Expr::Array(items) => {
+            // let Ok(items) = items.read() else {
+            //     // #todo maybe panic here?
+            //     return None;
+            // };
+            // let items = expect_lock_read(items);
+            Some(Rc::new(RefCell::new(ArrayIteratorRc2 {
+                current: 0,
+                items: items.clone(),
                 step: 1,
             })))
         }
